83a63dfb58706630efc247478ece104f79690a62
pappusaha <146526617+pappusaha@users.noreply.github.com>
2024-10-26 11:13:41 +0600

    changed name of the footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         
             <footer className="footer footer-center bg-base-300 text-base-content p-4 mb-0  ">
   <aside>
-    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by Pappu </p>
   </aside>
 </footer>
        
